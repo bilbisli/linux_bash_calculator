42fa14603db4ddc3f6d834328678c5bf54cb54d7
sawserlauz48 <sawserlauz48@gmail.com>
2023-08-17 17:19:20 +0300

    even_odd fix

--- math_operations/is_even_odd.sh
+++ math_operations/is_even_odd.sh
@@ -15,11 +15,11 @@ return 0
 
 function main(){
 		ansFromAns=$1
-#		echo "$ansFromAns"
 	if [[ $ansFromAns =~ ^[0-9]*\.[0-9]+$ ]]; then
 		 intger_after_float="${ansFromAns%%.*}"
-		 echo "$intger_after_float"
 		 echo "$(is_even_odd $intger_after_float)"
+	else 
+		 echo "$(is_even_odd $1)"
 	fi
 }
 
