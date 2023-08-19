d8a13797876c42be26bc2c83ca95e7fe46aba011
bilbisli <bilbisli@gmail.com>
2023-08-19 21:50:52 +0300

    fixed divide delete conflict and modified menu

--- menu.sh
+++ menu.sh
@@ -4,6 +4,22 @@ local ans=`expr $1 \* $2`
 echo "$ans";
 }
 
+operations=("Add" "Subtract" "Multiply" "Divide" "Power of" "Modulos" "Exit")
+scripts=("add" "subtract" "multiply" "divide" "power_of" "modulus")
+
+
+echo "Choose an operation to perform:"
+select operation in ${operations[@]}; do
+	case "$operation" in
+	"Add")		echo "Burger = 5\$"	;;
+	"Hotdogs")	echo "Hotdogs = 3\$"	;;
+	"Icecream")	echo "Icecream = 1.5\$"	;;
+	"Falafel")	echo "Falafel = 9\$"	;;
+	*)		echo "Wrong choice."	;;
+	esac
+	break
+done
+
 echo "
 a. Add
 b. Subtract
